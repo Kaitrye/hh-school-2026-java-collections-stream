cbf75cfe6e0411306e7daebba057694f57e502ea
Kaitrye <bogdanova.tekna156@yandex.ru>
2026-02-24 14:22:49 +0300

    Add solutions for tasks

--- src/main/java/tasks/Task9.java
+++ src/main/java/tasks/Task9.java
@@ -12,6 +12,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.Objects;
 
 /*
 Далее вы увидите код, который специально написан максимально плохо.
@@ -25,69 +26,104 @@ public class Task9 {
 
   // Костыль, эластик всегда выдает в топе "фальшивую персону".
   // Конвертируем начиная со второй
+
+  // Комментарий:
+  //    Чтобы не изменять входные данные и не использовать операцию remove(0) 
+  // (которая может быть дорогой при использовании ArrayList), 
+  // лучше использовать метод skip(1) для пропуска первого элемента потока. 
+  // Это эффективнее и безопаснее.
   public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
-    }
-    persons.remove(0);
-    return persons.stream().map(Person::firstName).collect(Collectors.toList());
+    return persons.stream().skip(1).map(Person::firstName).toList();
   }
 
   // Зачем-то нужны различные имена этих же персон (без учета фальшивой разумеется)
+  
+  // Комментарий:
+  //   Убрала ненужное использование Stream, 
+  // т.к. в данном случае достаточно создать HashSet из списка имен, 
+  // который будет содержать только уникальные значения в силу своей структуры. 
+  // Это упрощает код и улучшает его читаемость.
   public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
+    return new HashSet<>(getNames(persons));  
   }
 
   // Тут фронтовая логика, делаем за них работу - склеиваем ФИО
+
+  // Комментарий:
+  //    Исправила двойную конкатенацию secondName 
+  // на правильную последовательность: secondName, firstName, middleName.
+  // А также заменила ручную конкатенацию на использование Stream с Collectors.joining.
+  // Это делает код более чистым и читаемым.
   public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.secondName() != null) {
-      result += person.secondName();
-    }
-
-    if (person.firstName() != null) {
-      result += " " + person.firstName();
-    }
-
-    if (person.secondName() != null) {
-      result += " " + person.secondName();
-    }
-    return result;
+    return Stream.of(person.secondName(), person.firstName(), person.middleName())
+        .filter(Objects::nonNull)
+        .collect(Collectors.joining(" "));
   }
 
-  // словарь id персоны -> ее имя
+  // Словарь id персоны -> ее имя
+
+  // Комментарий:
+  //    Реализовала метод с помощью Collectors.toMap, который позволяет создать Map из потока.
+  // Это более лаконично и эффективно, чем ручное заполнение Map.
   public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.id())) {
-        map.put(person.id(), convertPersonToString(person));
-      }
-    }
-    return map;
+    return persons.stream()
+      .collect(Collectors.toMap(
+        Person::id,
+        this::convertPersonToString,
+        (existingValue, newValue) -> existingValue));
   }
 
-  // есть ли совпадающие в двух коллекциях персоны?
+  // Есть ли совпадающие в двух коллекциях персоны?
+
+  // Комментарий:
+  //    Вместо вложенного цикла для проверки наличия общих персон,
+  // можно создать HashSet из второй коллекции и использовать метод anyMatch для проверки наличия совпадений. 
+  // Это значительно улучшает производительность, особенно при больших коллекциях.
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
-        }
-      }
-    }
-    return has;
+    Set<Person> personsSet = new HashSet<>(persons2);
+    return persons1.stream().anyMatch(personsSet::contains);
   }
 
   // Посчитать число четных чисел
+
+  // Комментарий:
+  //    Убрала ненужное использование forEach и ручного счетчика,
+  // и заменила на использование метода count() после фильтрации четных чисел.
+  // Также использовала битовую операцию для проверки четности, 
+  // что может быть немного быстрее, чем использование оператора %.
   public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
+    return numbers.filter(num -> (num & 1) == 0).count();
   }
 
   // Загадка - объясните почему assert тут всегда верен
   // Пояснение в чем соль - мы перетасовали числа, обернули в HashSet, а toString() у него вернул их в сортированном порядке
+
+  // Комментарий:
+
+  //    Это происходит из-за особенности hashCode() для Integer (хэш числа - это его значение),
+  // созданию HashSet из списка, а не последовательного добавления с помощью add()
+  // и благодаря тому, что максимальное значение элементов списка 10000 не превосходит размера списка.
+
+  // Номер бакета, в который нужно положить элемент, 
+  // определяется как остаток от деления хэша элемента на размер table (массив бакетов). 
+  // Т.к. размер table будет 
+  // n = 10`000 / 0.75 (округленное в большую сторону до ближайшей степени двойки), 
+  // то для всех чисел от 1 до 10`000 хэш и будет являться номером бакета.
+  // А т.к. хэш для Integer - это само число и числа все различны, 
+  // то все элементы будут размещены в отдельных бакетах без коллизий.
+
+  // Поэтому получается, что при создании HashSet число 1 попадает в бакет table[1], 
+  // 2 - в table[2], 3 - в table[3], и т.д. 
+
+  // А при вызове функции toString() преобразование как раз происходит по порядку от бакета table[0] до бакета table[n-1].
+  // Поэтому и получится, что числа будут выведены в порядке возрастания.
+
+  // Если же изменить функцию hashCode(), 
+  // сделать последовательное добавление элементов 
+  // или добавить значения чисел, превышающие размер списка, 
+  // то порядок элементов будет отличаться от исходного, 
+  // т.к. номер бакета уже не будет соответствовать значениям чисел
+  // и могут возникнуть коллизии.
   void listVsSet() {
     List<Integer> integers = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toList());
     List<Integer> snapshot = new ArrayList<>(integers);
